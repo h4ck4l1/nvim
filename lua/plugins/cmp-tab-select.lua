ce4329282933558b9d177893efeb853d944f300b
h4ck4l1 <sohailmd123@gmail.com>
2025-12-10 16:13:43 +0000

    Tab completion in tandem with tab indent
    Added tab completion to go along with tab indent which was absent before

--- lua/plugins/cmp-tab-select.lua
+++ lua/plugins/cmp-tab-select.lua
@@ -1,14 +1,31 @@
 -- return { }
 
+-- return {
+--      {
+--          "saghen/blink.cmp",
+--          opts = function(_, opts)
+--              opts.keymap = {
+--                  preset = "super-tab",
+--                  ["<Tab>"] = { "select_and_accept" },
+--                  ["<S-Tab>"] = { "select_prev" },
+--              }
+--          end,
+--      },
+-- }
+--
+--
+--
+
+
 return {
-     {
-         "saghen/blink.cmp",
-         opts = function(_, opts)
-             opts.keymap = {
-                 preset = "super-tab",
-                 ["<Tab>"] = { "select_and_accept" },
-                 ["<S-Tab>"] = { "select_prev" },
-             }
-         end,
-     },
+    {
+        "saghen/blink.cmp",
+        opts = function(_, opts)
+            opts.keymap = {
+                preset = "super-tab",
+                ["<Tab>"] = { "select_and_accept", "fallback" },
+                ["<S-Tab>"] = { "select_prev", "fallback" },
+            }
+        end,
+    },
 }
