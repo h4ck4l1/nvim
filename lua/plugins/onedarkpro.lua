87999aba2c59c48914bf87f78a6c80affcfa8701
h4ck4l1 <sohailmd123@gmail.com>
2025-12-23 18:37:51 +0530

    add
    add

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -1,24 +1,40 @@
+
+
 return {
 	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- load early so colorscheme can be set safely
+		"navarasu/onedark.nvim",
+		priority = 1000,
 		config = function()
-			-- minimal custom setup (optional)
-			require("onedarkpro").setup({
-				-- only include fields you want to override — empty setup uses defaults
-				styles = {
-					comments = "italic",
-					keywords = "bold",
-				},
-				-- enable/disable built-in filetype/plugin groups if you want
-				plugins = {
-					gitsigns = true,
-					telescope = true,
-					nvim_tree = true,
-				},
-			})
-			-- pick a variant: "onedark" is the default; others: "onelight", "onedark_vivid", "vaporwave", "onedark_dark"
-			vim.cmd("colorscheme onedark_dark")
-		end,
+			require('onedark').setup {
+				style = 'darker'
+			}
+			require('onedark').load()
+		end
 	}
 }
+--
+--
+-- return {
+-- 	{
+-- 		"olimorris/onedarkpro.nvim",
+-- 		priority = 1000, -- load early so colorscheme can be set safely
+-- 		config = function()
+-- 			-- minimal custom setup (optional)
+-- 			require("onedarkpro").setup({
+-- 				-- only include fields you want to override — empty setup uses defaults
+-- 				styles = {
+-- 					comments = "italic",
+-- 					keywords = "bold",
+-- 				},
+-- 				-- enable/disable built-in filetype/plugin groups if you want
+-- 				plugins = {
+-- 					gitsigns = true,
+-- 					telescope = true,
+-- 					nvim_tree = true,
+-- 				},
+-- 			})
+-- 			-- pick a variant: "onedark" is the default; others: "onelight", "onedark_vivid", "vaporwave", "onedark_dark"
+-- 			vim.cmd("colorscheme onedark_dark")
+-- 		end,
+-- 	}
+-- }
