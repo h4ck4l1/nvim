40ba76b37008fc424f64198d0ef7e8d4915ee82a
h4ck4l1 <sohailmd123@gmail.com>
2026-08-15 19:58:51 +0530

    add
    add

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -21,12 +21,12 @@ return {
 				-- 1. Override the core color palette
 				colors = {
 					bg = "#0f1117",       -- Dark blue/black background
-					fg = "#94354e",       -- Soft light blue/white text
+					fg = "#7aa2f7",       -- Soft light blue/white text
 					gray = "#3b4261",     -- Darker gray for comments
 					red = "#1a86ad",      -- Soft red
 					green = "#9ece6a",    -- Pastel green
 					yellow = "#e0af68",   -- Soft yellow
-					blue = "#7aa2f7",     -- Bright blue
+					blue = "#8f6ff7",     -- Bright blue
 					purple = "#bb9af7",   -- Soft purple
 					cyan = "#7dcfff",     -- Bright cyan
 					cursorline = "#1a1b26", -- Background color of the line you are on
@@ -34,13 +34,13 @@ return {
 
 				-- 2. Customize how specific code elements are styled
 				highlights = {
-					Comment = { fg = "#565f89", style = "italic" }, 
-					Keyword = { fg = "#bb9af7", style = "bold" },   
-					Function = { fg = "#7aa2f7" },                   
-					String = { fg = "#9ece6a" },                    
-					Constant = { fg = "#ff9e64" },                  
-					CursorLineNr = { fg = "#7dcfff", style = "bold" }, 
-					LineNr = { fg = "#3b4261" },                     
+					Comment = { fg = "#565f89", style = "italic" },
+					Keyword = { fg = "#bb9af7", style = "bold" },
+					Function = { fg = "#7aa2f7" },
+					String = { fg = "#9ece6a" },
+					Constant = { fg = "#ff9e64" },
+					CursorLineNr = { fg = "#7dcfff", style = "bold" },
+					LineNr = { fg = "#3b4261" },
 
 					-- 1. Make standard variable references (like `tracing`) standard text color (#c0caf5)
 					["@variable"] = { fg = "${fg}" },
@@ -54,7 +54,7 @@ return {
 				-- 3. Adjust general styles easily
 				styles = {
 					types = "NONE",
-					methods = "NONE",
+					methods = "bold,italic",
 					numbers = "NONE",
 					strings = "NONE",
 					comments = "italic",
